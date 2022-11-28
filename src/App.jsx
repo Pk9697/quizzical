1846e29bc52dbb0f5ed50589edc2f4fa428c1894
Prashant Kumar <prashant.aman9@gmail.com>
2022-11-28 19:45:20 +0530

    apply logic to switch start page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Start from './components/Start'
+import QuizList from './components/QuizList'
 function App() {
-
+  const [isQuizStarted,setIsQuizStarted]=React.useState(false)
+  function startQuiz(){
+    setIsQuizStarted(prevState=>!prevState)
+  }
   return (
     <div className='App'>
-      <Start/>
+      {isQuizStarted?
+      <QuizList/>
+      :
+      <Start startQuiz={startQuiz}/>
+      }
     </div>
   )
 }
--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-export default function Start(){
+export default function Start(props){
     return (
         <div className="start--div">
             <h1>Quizzical</h1>
             <p>Start quiz when you are ready</p>
-            <button className="start--btn">Start quiz</button>
+            <button onClick={props.startQuiz} className="start--btn">Start quiz</button>
         </div>
     )
 }
