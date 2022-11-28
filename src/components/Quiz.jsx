84b438ff2867780821a7e15d1c61d1b7b594561c
Prashant Kumar <prashant.aman9@gmail.com>
2022-11-28 20:40:00 +0530

    style quizlist and quiz page

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Quiz() {
+  return (
+    <div className='quiz'>
+        <h3>How would one say goodbye in Spanish?</h3>
+        <div className="quiz--options">
+            <button>Adios</button>
+            <button>Hola</button>
+            <button>Au Revior</button>
+            <button>Salir</button>
+        </div>
+    </div>
+  )
+}
--- src/components/QuizList.jsx
+++ src/components/QuizList.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
-
+import Quiz from './Quiz'
 export default function QuizList() {
   return (
-    <div>QuizList</div>
+    <div className='quiz--list'>
+        <div>
+          <Quiz/>
+          <Quiz/>
+          <Quiz/>
+          <Quiz/>
+          <Quiz/>
+        </div>
+        <button className='quiz--check--btn'>Check answers</button>
+    </div>
+
   )
 }
