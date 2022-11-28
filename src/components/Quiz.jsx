ecba73d3330428e828a8041228e493d5a49c5645
Prashant Kumar <prashant.aman9@gmail.com>
2022-11-28 21:34:08 +0530

    fetch api and display data

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,14 +1,32 @@
 import React from 'react'
 
-export default function Quiz() {
+export default function Quiz(props) {
+    function getRandomAnswers(){
+        const answers=props.incorrect_answers
+        const correctAnswer=props.correct_answer
+        const randomIndex=Math.floor(Math.random()*(answers.length+1))
+        // console.log(randomIndex)
+        const newAnswers=[]
+        let idx=0;
+        for(let i=0;i<answers.length+1;i++){
+            if(i!=randomIndex){
+                newAnswers.push(answers[idx++])
+            }else{
+                newAnswers.push(correctAnswer)
+            }
+        }
+        // console.log(newAnswers)
+        return newAnswers
+    }
+    const answerElements=getRandomAnswers().map((answer,index)=>{
+        return <button key={index}>{answer}</button>
+    })
+    // console.log(answerElements)
   return (
     <div className='quiz'>
-        <h3>How would one say goodbye in Spanish?</h3>
+        <h3>{props.question}</h3>
         <div className="quiz--options">
-            <button>Adios</button>
-            <button>Hola</button>
-            <button>Au Revior</button>
-            <button>Salir</button>
+            {answerElements}
         </div>
     </div>
   )
--- src/components/QuizList.jsx
+++ src/components/QuizList.jsx
@@ -1,14 +1,33 @@
 import React from 'react'
 import Quiz from './Quiz'
 export default function QuizList() {
+
+  const [quizData,setQuizData]=React.useState([])
+  
+  // console.log("QuizList comp rendered")
+  // console.log(quizData)
+  React.useEffect(()=>{
+    fetch('https://opentdb.com/api.php?amount=5')
+      .then(res=>res.json())
+      .then(data=>(
+        setQuizData(data.results.map((quiz,index)=>{
+          return {
+            ...quiz,
+            id:`quiz-${index}`,
+            selectedAnswer:""
+          }
+        }))
+      ))
+  },[])
+
   return (
     <div className='quiz--list'>
         <div>
-          <Quiz/>
-          <Quiz/>
-          <Quiz/>
-          <Quiz/>
-          <Quiz/>
+          {
+            quizData.map(quiz=>{
+              return <Quiz key={quiz.id} {...quiz}/>
+            })
+          }
         </div>
         <button className='quiz--check--btn'>Check answers</button>
     </div>
